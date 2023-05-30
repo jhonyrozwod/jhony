2cbca69e89a524d7ca1eb9336ac32c49091798cb
jhonyrozwod <jhonathanrozwod@gmail.com>
2023-05-30 20:41:59 -0300

    va22222333442i

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('./src/app');
 console.log(process.env.port);
 const port = process.env.port || 3000;
 
-header('Access-Control-Allow-Origin: *');
 
 app.listen(port, () => {
 console.log('Aplicação executando na porta...: ', port);
