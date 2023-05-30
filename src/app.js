2748693267ee49738eca672d6f396798cfd9f283
jhonyrozwod <jhonathanrozwod@gmail.com>
2023-05-30 20:41:04 -0300

    va2222i

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const morgan = require('morgan');
 const mongooseConnection = require('./config/mongooseConnection.config');
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(morgan('dev'));
-app.use(cors());
+//app.use(cors());
 
 // ==> Retornando a conexão via mongoose via external file usando 'app.set()'
 app.set('mongoose connection', mongooseConnection);
